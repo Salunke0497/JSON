b91d007eef882f8e5ccdcfa5a093a60dbac97c65
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-29 11:39:18 +0530

    welcome

---
+++ Details.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stock
+{
+    internal class Details
+    {
+            public List<AutoSector>AutoSector;
+            public List<BankSector>BankSector;
+            public List<FMCG>FMCG;
+
+    }
+    public class AutoSector
+    {
+        public string name;
+        public int Quantity;
+        public int price;
+    }
+    public class BankSector
+    {
+        public string name;
+        public int Quantity;
+        public int price;
+    }
+    public class FMCG
+    {
+        public string name;
+        public int Quantity;
+        public int price;
+    }
+}
